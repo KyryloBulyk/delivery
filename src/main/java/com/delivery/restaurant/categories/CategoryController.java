f7105886ae10abc4ca354ca47b87a27050d0b277
Kyrylo <kyrylo@ip-154-201.wifi.tuke.sk>
2023-12-15 09:42:21 +0100

    Add controller, service and repository for Category

--- src/main/java/com/delivery/restaurant/categories/CategoryController.java
+++ src/main/java/com/delivery/restaurant/categories/CategoryController.java
@@ -1,12 +1,46 @@
 package com.delivery.restaurant.categories;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/categories")
 public class CategoryController {
     private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category createdCategory = categoryService.createCategory(category);
+        return ResponseEntity.ok(createdCategory);
+    }
+
+    @PutMapping("/changing/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category categoryDetails) {
+        Category updatedCategory = categoryService.updateCategory(id, categoryDetails);
+        if (updatedCategory == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/deleting/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        boolean isDeleted = categoryService.deleteCategory(id);
+        if (!isDeleted) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/delivery/restaurant/categories/CategoryService.java
+++ src/main/java/com/delivery/restaurant/categories/CategoryService.java
@@ -2,11 +2,40 @@ package com.delivery.restaurant.categories;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Long id, Category categoryDetails) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category == null) {
+            return null;
+        }
+        category.setName(categoryDetails.getName());
+        return categoryRepository.save(category);
+    }
+
+    public boolean deleteCategory(Long id) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category == null) {
+            return false;
+        }
+        categoryRepository.delete(category);
+        return true;
     }
 }
