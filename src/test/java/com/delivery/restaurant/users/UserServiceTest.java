0d5387f450bf36ca5d4ca8ef729a93f4597e1d18
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2023-12-19 13:02:06 +0100

    Create Tests for UserService

--- src/test/java/com/delivery/restaurant/users/UserServiceTest.java
+++ src/test/java/com/delivery/restaurant/users/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.delivery.restaurant.users;
 
+import com.delivery.restaurant.cart.Cart;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,9 +9,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,26 +22,154 @@ class UserServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    @InjectMocks
     private UserService userService;
+
+    private User user;
 
     @BeforeEach
     void setUp() {
-        userService = new UserService(userRepository);
+        user = new User(1, "Test User", "test@example.com", "password", new Cart());
     }
 
     @Test
     void getAllUsers() {
         //given
-        User user1 = new User(1, "User 1", "user1@example.com", "password123", new HashSet<>());
-        User user2 = new User(2, "User 2", "user2@example.com", "password456", new HashSet<>());
-
-        List<User> expectedUsers = Arrays.asList(user1, user2);
+        User anotherUser =  new User(2, "Another User", "another@example.com", "password", new Cart());
+        List<User> expectedUsers = Arrays.asList(user, anotherUser);
+        Mockito.when(userRepository.findAll()).thenReturn(expectedUsers);
 
         //when
-        Mockito.when(userRepository.findAll()).thenReturn(expectedUsers);
         List<User> actualUsers = userService.getAllUsers();
 
         //then
         assertEquals(expectedUsers, actualUsers);
+        Mockito.verify(userRepository).findAll();
     }
+
+    @Test
+    void createUser() {
+        //given
+        Mockito.when(userRepository.findByEmail(user.getEmail())).thenReturn(java.util.Optional.empty());
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+
+        //when
+        User actualUser = userService.createUser(user);
+
+        //then
+        assertEquals(user, actualUser);
+        Mockito.verify(userRepository).findByEmail(user.getEmail());
+        Mockito.verify(userRepository).save(user);
+    }
+
+    @Test
+    void createUserWithExistingUser() {
+        //given
+        Mockito.when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+        //when
+        User resultUser = userService.createUser(user);
+
+        //then
+        Mockito.verify(userRepository).findByEmail(user.getEmail());
+        assertNull(resultUser);
+    }
+
+    @Test
+    void updateUser() {
+        // given
+        User existingUser = new User(1, "Existing User", "existing@example.com", "password", new Cart());
+        User updateUser = new User(1, "Another User", "another@example.com", "password123", new Cart());
+
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(existingUser));
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(updateUser);
+
+        // when
+        User actualUser = userService.updateUser(1L, updateUser);
+
+        // then
+        assertNotNull(actualUser);
+        assertEquals(updateUser.getName(), actualUser.getName());
+        assertEquals(updateUser.getEmail(), actualUser.getEmail());
+        assertEquals(updateUser.getPassword(), actualUser.getPassword());
+
+        Mockito.verify(userRepository).findById(1L);
+        Mockito.verify(userRepository).save(Mockito.any(User.class));
+    }
+
+    @Test
+    void updateUserWhenUserEqualsNull() {
+        //given
+        User updateUser = new User(1, "Another User", "another@example.com", "password123", new Cart());
+
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        //when
+        User actualUser = userService.updateUser(1L, updateUser);
+
+        //then
+        assertNull(actualUser);
+        Mockito.verify(userRepository).findById(1L);
+    }
+
+    @Test
+    void deleteUser() {
+        //given
+        User existingUser = new User(1, "Existing User", "existing@example.com", "password", new Cart());
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(existingUser));
+
+        //when
+        boolean result = userService.deleteUser(1L);
+
+        //then
+        assertTrue(result);
+        Mockito.verify(userRepository).findById(1L);
+        Mockito.verify(userRepository).delete(existingUser);
+    }
+
+    @Test
+    void deleteUserWhenUserEqualsNull() {
+        //given
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        //when
+        boolean result = userService.deleteUser(1L);
+
+        //then
+        assertFalse(result);
+        Mockito.verify(userRepository).findById(1L);
+    }
+
+    @Test
+    void authenticateUser() {
+        //given
+        User testUser = new User(1, "Test User", "test@example.com", "password", new Cart());
+        String testEmail = "test@example.com";
+        String testPassword = "password";
+
+        Mockito.when(userRepository.findByEmail(testEmail)).thenReturn(Optional.of(testUser));
+
+        //when
+        User resultUser = userService.authenticateUser(testEmail, testPassword);
+
+        //then
+        Mockito.verify(userRepository).findByEmail(testEmail);
+        assertEquals(testUser, resultUser);
+    }
+
+    @Test
+    void authenticateUserWhenUserEqualsNull() {
+        //given
+        String testEmail = "test@example.com";
+        String testPassword = "password";
+        Mockito.when(userRepository.findByEmail(testEmail)).thenReturn(Optional.empty());
+
+        //when
+        User resultUser = userService.authenticateUser(testEmail, testPassword);
+
+        //then
+        Mockito.verify(userRepository).findByEmail(testEmail);
+        assertNull(resultUser);
+    }
+
 }
