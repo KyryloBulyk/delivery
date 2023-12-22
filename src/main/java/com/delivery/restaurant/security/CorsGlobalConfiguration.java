29275e06ae78d2aac434b77a79665dd828891cc9
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2023-12-22 22:22:32 +0100

    Add configuration for Frontend

---
+++ src/main/java/com/delivery/restaurant/security/CorsGlobalConfiguration.java
@@ -0,0 +1,25 @@
+package com.delivery.restaurant.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173"); // Allow your frontend origin
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Apply CORS configuration for all paths
+
+        return new CorsFilter(source);
+    }
+}
