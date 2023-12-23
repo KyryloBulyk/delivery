5a5143898960329ec7254be987b33dd538f82f78
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2023-12-23 13:57:56 +0100

    Create Integration Tests for CategoryController and refactor Integration Tests for ProductController

--- src/test/java/com/delivery/restaurant/categories/CategoryControllerIntegrationTest.java
+++ src/test/java/com/delivery/restaurant/categories/CategoryControllerIntegrationTest.java
@@ -1,12 +1,8 @@
-package com.delivery.restaurant.products;
+package com.delivery.restaurant.categories;
 
-import com.delivery.restaurant.categories.Category;
-import com.delivery.restaurant.categories.CategoryRepository;
 import com.delivery.restaurant.users.User;
 import com.delivery.restaurant.users.UserRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ProductControllerIntegrationTest {
+public class CategoryControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -37,15 +33,13 @@ public class ProductControllerIntegrationTest {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @Autowired
-    private ProductRepository productRepository;
-
     private String token;
-
-    private Long productId;
 
     @BeforeEach
     public void setup() throws Exception {
+        userRepository.deleteAll();
+        categoryRepository.deleteAll();
+
         User testUser = new User();
         testUser.setName("Test User");
         testUser.setEmail("test@example.com");
@@ -54,26 +48,6 @@ public class ProductControllerIntegrationTest {
         userRepository.save(testUser);
 
         this.token = authenticateAndGetToken("test@example.com", "password");
-
-        Category category = new Category();
-        category.setName("Test Category");
-        Category savedCategory = categoryRepository.save(category);
-
-        Product product = new Product();
-        product.setTitle("Test Product");
-        product.setPrice(10.0);
-        product.setImg("url");
-        product.setCategory(savedCategory);
-        Product savedProduct = productRepository.save(product);
-
-        this.productId = savedProduct.getId();
-    }
-
-    @AfterEach
-    void tearDown() {
-        userRepository.deleteAll();
-        productRepository.deleteAll();
-        categoryRepository.deleteAll();
     }
 
     private String authenticateAndGetToken(String username, String password) throws Exception {
@@ -89,38 +63,45 @@ public class ProductControllerIntegrationTest {
     }
 
     @Test
-    public void getAllProducts() throws Exception {
-        mockMvc.perform(get("/api/v1/products")
+    public void getAllCategories() throws Exception {
+        mockMvc.perform(get("/api/v1/categories")
                         .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
-    public void createProduct() throws Exception {
-        long categoryId = categoryRepository.findAll().get(0).getId();
-        String productJson = "{\"title\":\"New Product\",\"price\":10.0,\"img\":\"url\",\"categoryId\":" + categoryId + "}";
+    public void createCategory() throws Exception {
+        String categoryJson = "{\"name\":\"New Category\"}";
 
-        mockMvc.perform(post("/api/v1/products/create")
+        mockMvc.perform(post("/api/v1/categories/create")
                         .header("Authorization", "Bearer " + token)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(productJson))
+                        .content(categoryJson))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void updateProduct() throws Exception {
-        String updatedProductJson = "{\"title\":\"Updated Product\",\"price\":15.0,\"img\":\"updatedUrl\",\"categoryId\":1}";
-        mockMvc.perform(put("/api/v1/products/changing/{id}", 1)
+    public void updateCategory() throws Exception {
+        Category category = new Category();
+        category.setName("Old Category");
+        Category savedCategory = categoryRepository.save(category);
+
+        String updatedCategoryJson = "{\"name\":\"Updated Category\"}";
+        mockMvc.perform(put("/api/v1/categories/changing/{id}", savedCategory.getId())
                         .header("Authorization", "Bearer " + token)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(updatedProductJson))
+                        .content(updatedCategoryJson))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void deleteProduct() throws Exception {
-        mockMvc.perform(delete("/api/v1/products/deleting/{id}", productId) // Використовуйте збережений ID
+    public void deleteCategory() throws Exception {
+        Category category = new Category();
+        category.setName("Category to Delete");
+        Category savedCategory = categoryRepository.save(category);
+
+        mockMvc.perform(delete("/api/v1/categories/deleting/{id}", savedCategory.getId())
                         .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk());
     }
